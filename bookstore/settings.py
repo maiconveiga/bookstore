a79bf16d6e979917bf2874a965f0c8273fba674d
= <=>
2024-06-17 22:20:00 -0300

    feat: Created a dockerfile and installed git into container. The dockfile was updated also.

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -31,7 +31,7 @@ if 'test' in sys.argv:
     DEBUG = False
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ebac-bookstore-api.herokuapp.com', 'drsantos20.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -140,9 +140,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
